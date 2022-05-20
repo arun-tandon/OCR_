49bf2a11f9f92bb74fd5bfc07ea1c592e31ed168
arun-tandon <60617403+arun-tandon@users.noreply.github.com>
2022-05-20 13:21:00 +0530

    Add files via upload

---
+++ OCR.py
@@ -0,0 +1,57 @@
+from PIL import Image
+import pytesseract
+import cv2
+import numpy as np
+import sys
+sys.path.append('../Module-1')
+from voice import *
+sys.path.append('../')
+
+def textRecognition(filename):
+  img1 = np.array(Image.open(filename))
+  text = pytesseract.image_to_string(img1)
+  l=''.join(text)
+  l=l.split('\n')
+  li=[]
+  for s in l:
+    s1="".join(c for c in s if c.isalpha() or c.isdigit() or c==" " or c in "!@#$%&*.?':;_" )
+    if s1!="":
+      li.append(s1) 
+  temp=" ".join(li)
+  temp=temp.split('.')
+  return temp
+  
+def ocr(img):
+  sentences=textRecognition(img)
+  for s in sentences:
+    count=sum(len(x) for x in s.split())
+    print("length:")
+    print(count)
+    if count<=2: continue
+    voice(s)
+ 
+
+count = 0
+def cam():
+    global mode
+    global count
+    # Load Camera
+    cap = cv2.VideoCapture(0)
+    counter=0
+    while True:
+        if(counter%5!=0):
+            continue
+        ret, frame = cap.read()
+        nm="frame"+str(count)+".jpg"
+        cv2.imwrite(nm, frame)
+        count+=1
+        
+        ocr(nm)
+        cv2.imshow("frame",frame)
+        
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    cam()
